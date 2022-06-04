95b53b474f958a9423cf8026717d0d6a0bdadedf
nagarjunjeeva <nagarjunjeeva98@gmail.com>
2022-06-04 20:47:38 +0530

    Done

--- src/org/sample/GitSample1.java
+++ src/org/sample/GitSample1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class GitSample1 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your Name");
 		System.out.println("Enter a Number");
 		int num=sc.nextInt();
 		
